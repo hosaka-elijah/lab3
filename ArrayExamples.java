76b8420fe96a032203561cea2ccccaa2738614ad
hosaka-elijah <81043167+hosaka-elijah@users.noreply.github.com>
2023-01-25 13:49:51 -0800

    Add test and fixed reversed() and reverseInPlace()

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,19 +4,29 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
-    for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[arr.length - i - 1];
+    // for(int i = 0; i < arr.length; i += 1) {
+    //   arr[i] = arr[arr.length - i - 1];
+    // }
+    for (int i = 0; i < arr.length / 2; i++) {
+      int temp = arr[arr.length - i -1];
+      arr[arr.length - i - 1] = arr[i];
+      arr[i] = temp;
     }
   }
 
   // Returns a *new* array with all the elements of the input array in reversed
   // order
   static int[] reversed(int[] arr) {
+    // int[] newArray = new int[arr.length];
+    // for(int i = 0; i < arr.length; i += 1) {
+    //   arr[i] = newArray[arr.length - i - 1];
+    // }
+    // return arr;
     int[] newArray = new int[arr.length];
-    for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+    for (int i = 0; i < arr.length; i++) {
+      newArray[i] = arr[arr.length - i - 1];
     }
-    return arr;
+    return newArray;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
--- ArrayTests.java
+++ ArrayTests.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
 import org.junit.*;
 
 public class ArrayTests {
@@ -14,5 +17,18 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverseInPlace2() {
+    int[] input1 = {3, 6, 9};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{9,6,3}, input1);
+  }
+
+  @Test
+  public void testReverse2() {
+    int[] input1 = {3, 6, 9};
+    assertArrayEquals(new int[]{9,6,3}, ArrayExamples.reversed(input1));
   }
 }
