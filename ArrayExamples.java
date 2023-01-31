b8f215b3cd10226beed28baed34572e91b040b18
hosaka-elijah <81043167+hosaka-elijah@users.noreply.github.com>
2023-01-30 19:20:13 -0800

    Updated reverseInPlace() method in ArrayExamples.java

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -8,9 +8,9 @@ public class ArrayExamples {
     //   arr[i] = arr[arr.length - i - 1];
     // }
     for (int i = 0; i < arr.length / 2; i++) {
-      int temp = arr[arr.length - i -1];
-      arr[arr.length - i - 1] = arr[i];
-      arr[i] = temp;
+      int temp = arr[i];
+      arr[i] = arr[arr.length - i - 1];
+      arr[arr.length - i - 1] = temp;
     }
   }
 
